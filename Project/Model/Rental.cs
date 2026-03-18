7bf3873b623635ff7e5aa8bb9b40b2013d90d62a
Matt-Matthew <mateusz.jaworski04@gmail.com>
2026-03-18 17:58:22 +0100

    Dodalem klase Rental z obsluga dat i sprawdzaniem terminu zwrotu oraz zaczolem prace nad directory Service (modele zostaly juz ukonczone)

---
+++ Project/Model/Rental.cs
@@ -0,0 +1,37 @@
+namespace Project.Model;
+
+public class Rental
+{
+    private static int _nextId = 1;
+    public int Id { get; private set; }
+    public User Rent { get; private set; }
+    public Equipment RentedItem { get; private set; }
+    public DateTime DateRent { get; private set; }
+    public DateTime DateReturn { get; private set; }
+    public DateTime? ActualReturn { get; set; }
+
+    public bool IsReturnedOnTime
+    {
+        get
+        {
+            if (ActualReturn == null)
+            {
+                return false;
+            }
+            return ActualReturn <= DateReturn;
+        }
+        
+    }
+    public decimal AdditionalFee { get; set; }
+
+    public Rental(User rent, Equipment rentedItem, int daysRented)
+    {
+        Id = _nextId;
+        _nextId++;
+        
+        Rent = rent;
+        RentedItem = rentedItem;
+        DateRent = DateTime.Now;
+        DateReturn = DateRent.AddDays(daysRented);
+    }
+}
---
+++ Project/Service/Inventory.cs
@@ -0,0 +1,6 @@
+namespace Project.Service;
+
+public class Inventory
+{
+    
+}
