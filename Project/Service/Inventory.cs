78212689a588fd3590fad7551f8acbb0fb67fdf6
Matt-Matthew <mateusz.jaworski04@gmail.com>
2026-03-18 18:10:56 +0100

    Dodalem cala logike do klasy Inventory ktora zawiera teraz metody dajace mozliwosc sprawdzenia danego produktu,dodanie do listy,sprawdzenie dostepnosci i wiele innych

--- Project/Service/Inventory.cs
+++ Project/Service/Inventory.cs
@@ -1,6 +1,57 @@
+using Project.Model;
+
 namespace Project.Service;
 
 public class Inventory
 {
+    private List<Equipment> _equipmentList = new List<Equipment>();
+
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipmentList.Add(equipment);
+    }
+    public void DisplayInventory()
+    {
+        foreach (var equipment in _equipmentList)
+        {
+           Console.WriteLine($"Id: {equipment.Id} Name: {equipment.Name} , IsAvailable: {equipment.IsAvailable}");
+        }
+    }
+    
+    public void DisplayAvailableEquipment()
+    {
+        foreach (var equipment in _equipmentList)
+        {
+            if (equipment.IsAvailable)
+            {
+                Console.WriteLine($"Id: {equipment.Id} Name: {equipment.Name}");
+            }
+        }
+    }
+
+    public void MakeUnavailable(int equipmentId)
+    {
+        foreach (var euqipment in _equipmentList)
+        {
+            if (euqipment.Id == equipmentId)
+            {
+                euqipment.IsAvailable = false;
+                break;
+            }
+        }
+            
+    }
+
+    public Equipment GetItemById(int equipmentId)
+    {
+        foreach (var equipment in _equipmentList)
+        {
+            if (equipmentId == equipment.Id)
+            {
+                return equipment;
+            }
+        }
+        return null;
+    }
     
 }
